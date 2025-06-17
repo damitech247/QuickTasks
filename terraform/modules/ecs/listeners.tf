4297116769c806535b93d8474b86645a5d8ca28b
okon-emma <okon.emma0@gmail.com>
2025-06-17 07:45:02 -0500

    fix: add a forward rule to listener

--- terraform/modules/ecs/listeners.tf
+++ terraform/modules/ecs/listeners.tf
@@ -24,11 +24,7 @@ resource "aws_lb_listener" "https_listener" {
   certificate_arn = var.acm_arn
 
   default_action {
-    type = "fixed-response"
-    fixed_response {
-      content_type = "text/plain"
-      message_body = "Not Found"
-      status_code  = "200"
-    }
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.quicktasks.arn
   }
 }
