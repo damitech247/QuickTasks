e152265d2a4cbc31c30b731ecd6b1c12fa7e7e37
okon-emma <okon.emma0@gmail.com>
2025-06-17 08:43:24 -0500

    fix: Add SSL to Sequelize Config

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,5 +1,7 @@
 const { Sequelize } = require("sequelize");
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const db = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
@@ -8,6 +10,14 @@ const db = new Sequelize(
         host: process.env.DB_HOST,
         dialect: "postgres",
         logging: false,
+        dialectOptions: isProduction
+            ? {
+                ssl: {
+                    require: true,
+                    rejectUnauthorized: false, // use true with a verified cert
+                },
+            }
+            : {},
     }
 );
 
